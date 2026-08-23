492a494e32d4dbca34bf52017c54db3489c9842d
fuyu3 <pedrohenriquelemossilva8@gmail.com>
2026-08-22 22:19:33 -0300

    modified:   modules/boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,8 +2,15 @@
 
 {
 
-  # Usa systemd-boot em instalações UEFI.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    devices = [ "nodev" ];
+    efiSupport = true;
+    useOSProber = true;
+
+    # Define a resolução do menu de boot do GRUB.
+    gfxmodeEfi = "1920x1080x32";     
+  };
 
   # Permite criar e atualizar a entrada de boot na partição EFI.
   boot.loader.efi.canTouchEfiVariables = true;
